9ef3af056ba3ba95a5adcee24f049171b19e87ca
Long Thinh <lelongthinh1324@gmail.com>
2024-03-30 01:58:27 +1100

    Stop tracking build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/lelongthinh/rs2_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lelongthinh/universal_robot_ws/devel;/home/lelongthinh/rs2_ws/devel;/home/lelongthinh/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/lelongthinh/universal_robot_ws/devel;/home/lelongthinh/rs2_ws/devel;/home/lelongthinh/catkin_ws/devel;/opt/ros/noetic' != '' else []
