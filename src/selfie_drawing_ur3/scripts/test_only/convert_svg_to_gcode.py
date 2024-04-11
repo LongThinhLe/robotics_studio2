6520aa96090400aff629592839553277afd459a3
Long Thinh <lelongthinh1324@gmail.com>
2024-04-11 16:44:23 +1000

    update stop and pause threading

--- src/selfie_drawing_ur3/scripts/test_only/convert_svg_to_gcode.py
+++ src/selfie_drawing_ur3/scripts/test_only/convert_svg_to_gcode.py
@@ -16,12 +16,12 @@ def convert_svg_to_gcode(svg_file_name, gcode_file_name):
 
     # Parse an svg file into geometric curves, and compile to gcode
     curves = parse_file(svg_file_path)
-    # reordered_curves = reorder_curves_greedy(curves)
-    reordered_curves = solve_tsp(curves)
+    reordered_curves = reorder_curves_greedy(curves)
+    # reordered_curves = solve_tsp(curves)
     gcode_compiler.append_curves(reordered_curves)
 
     # Compile the G-code and save it directly to a file
     gcode_compiler.compile_to_file(gcode_file_path)
 
 if __name__ == "__main__":
-    convert_svg_to_gcode("drawing.svg", "drawing.gcode")
+    convert_svg_to_gcode("/home/lelongthinh/rs2_ws/img/outline_picture_rmbg.svg", "/home/lelongthinh/rs2_ws/gcode/drawing.gcode")
